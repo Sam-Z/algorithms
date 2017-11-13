b11155465f63cd4e50583dbe61e15b7bba9494cd
zhangdi <765350744@qq.com>
2017-11-13 21:20:29 +0800

    merge_sort_bu ok

--- sort/sort.c
+++ sort/sort.c
@@ -97,10 +97,36 @@ void __merge(int *buffer, int l, int r, int m)
     }
 }
 
+// 
+int __insert_sort(int *buffer, int l, int r)
+{
+    // i 表示待插入的数
+    for (int i=l+1; i<=r; i++)
+    {
+        int tmp = buffer[i];
+
+        // j表示下一个插入的位置
+        int j = i-1;
+        for (; j>=l; j--)
+        {
+            if (buffer[j]>tmp)
+            {
+                buffer[j+1]=buffer[j];
+            }
+            {
+                break;
+            }
+        }
+        buffer[j+1]=tmp;
+    }
+    
+}
+
 void __merge_sort(int *buffer, int l, int r)
 {
-    if (l>=r)
+    if (r-l<15)
     {
+        __insert_sort(buffer, l, r);
         return;
     }
 
@@ -108,12 +134,29 @@ void __merge_sort(int *buffer, int l, int r)
 
     __merge_sort(buffer, l, m);
     __merge_sort(buffer, m+1, r);
+    if (buffer[m]>buffer[m+1])
     __merge(buffer, l,r,m);
 }
 
 int merge_sort(int *buffer, int size)
 { 
     __merge_sort(buffer, 0, size-1);
+}
+//
+//自低向下递推归并排序
+//
+int merge_sort_bu(int *buffer, int size)
+{
+    for (int l=1; l<=size; l*=2)
+    {
+        //对buffer[i...i+l-1], buffer[i+l+1...i+2*l+1]归并
+        for(int i=0; i<size-l+1; i += 2*l)
+        {
+            __merge(buffer, i, i+2*l-1<size-1?i+2*l-1:size-1,  i+l-1);
+        }
+    }
+
+    return (0);
 }
 
 int __partition(int *buffer, int l, int r)
--- sort/sort.h
+++ sort/sort.h
@@ -7,5 +7,5 @@ int insert_sort(int *buffer, int size);
 int merge_sort(int *buffer, int size);
 int quick_sort(int *buffer, int size);
 int shell_sort(int *buffer, int size);
-
+int merge_sort_bu(int *buffer, int size);
 #endif
